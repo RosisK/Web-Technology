97b91c59623cd1df3e73153e1e04e19fd9292d93
RosisK <rosiskharel5@gmail.com>
2025-02-18 19:50:09 +0545

    Another commit

--- fluf/script.js
+++ fluf/script.js
@@ -4,22 +4,33 @@ const container = document.querySelector(".mega-container");
 
 function addItem() {
     // Creating elements
-    const item = document.createElement("div");
-    const para = document.createElement("p");
+    const itemDiv = document.createElement("div");
+    const itemLeftDiv = document.createElement("div");
+    const itemRightDiv = document.createElement("div");
+    const checkBox = document.createElement("input");
+    const taskText = document.createElement("p");
     const deleteBtn = document.createElement("button");
     const editBtn = document.createElement("button");
-    const checkBox = document.createElement("input");
-
+    
     // Assigning properties
     checkBox.type = "checkbox";
-    
-    
-    item.appendChild(checkBox);
-    item.appendChild(para);
-    item.classList.add("task-item");
-    para.textContent = inputBox.value;
-    container.appendChild(item);
 
+    itemDiv.classList.add("task-item");
+
+    deleteBtn.classList.add("taskEditBtn");
+    deleteBtn.textContent = "Delete";
+    editBtn.textContent = "Edit";
+    editBtn.classList.add("taskEditBtn");
+    taskText.textContent = inputBox.value;
+    
+    itemDiv.appendChild(itemLeftDiv);
+    itemDiv.appendChild(itemRightDiv);
+    itemLeftDiv.appendChild(checkBox);
+    itemLeftDiv.appendChild(taskText);
+    itemRightDiv.appendChild(deleteBtn);
+    itemRightDiv.appendChild(editBtn);
+
+    container.appendChild(itemDiv);
 }
 
 inputBtn.addEventListener("click", addItem);
