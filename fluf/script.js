f41b14d683f6eccd3456f1a192723aaf82d4fbbb
RosisK <rosiskharel5@gmail.com>
2025-02-18 19:37:18 +0545

    Another commit

--- fluf/script.js
+++ fluf/script.js
@@ -3,10 +3,17 @@ const inputBtn = document.getElementById("input-btn");
 const container = document.querySelector(".mega-container");
 
 function addItem() {
+    // Creating elements
     const item = document.createElement("div");
     const para = document.createElement("p");
+    const deleteBtn = document.createElement("button");
+    const editBtn = document.createElement("button");
     const checkBox = document.createElement("input");
+
+    // Assigning properties
     checkBox.type = "checkbox";
+    
+    
     item.appendChild(checkBox);
     item.appendChild(para);
     item.classList.add("task-item");
